9cff0cf357d4a1b96f373103083bf78b2b920dac
Michael Swanson <mikeswanson12@msn.com>
2014-03-14 17:35:48 -0400

    3rd challange prints a list of files in a given directory, filtered by the extension of the files, both passed in as arguments

---
+++ learn_node4.js
@@ -0,0 +1,23 @@
+var fs = require('fs');
+
+
+var fileCharArray = [];
+var pathBufferObject = [];
+
+
+pathBufferObject = process.argv;
+
+fs.readdir(pathBufferObject[2], function callback (err, data){
+
+	if (err){
+		throw err;
+	} else{
+		for( var i = 0; i< data.length; i++ ){
+			if (pathBufferObject[3] == data[i].split('.').pop() && (data[i].split('.').length == 2)){
+				console.log(data[i]);
+			}
+		}
+	}
+});
+
+
