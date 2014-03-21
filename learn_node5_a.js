0adc9b9bc439d8371683cc8e38fe62ff2a2052fa
Michael Swanson <mikeswanson12@msn.com>
2014-03-21 14:15:15 -0400

    turns module function into callable function. moves variables to original program file

---
+++ learn_node5_a.js
@@ -0,0 +1,7 @@
+var readAndFilter = require('./learn_node5_module.js')
+
+var fileCharArray = [];
+var pathBufferObject = [];
+
+
+pathBufferObject = process.argv;
--- learn_node5_module.js
+++ learn_node5_module.js
@@ -1,21 +1,19 @@
 var fs = require('fs');
 
 
-var fileCharArray = [];
-var pathBufferObject = [];
 
 
-pathBufferObject = process.argv;
+module.exports = function(args) {
+	fs.readdir(args[2], function callback (err, data){
 
-fs.readdir(pathBufferObject[2], function callback (err, data){
-
-	if (err){
-		throw err;
-	} else{
-		for( var i = 0; i< data.length; i++ ){
-			if (pathBufferObject[3] == data[i].split('.').pop() && (data[i].split('.').length == 2)){
-				console.log(data[i]);
+		if (err){
+			throw err;
+		} else{
+			for( var i = 0; i< data.length; i++ ){
+				if (args[3] == data[i].split('.').pop() && (data[i].split('.').length == 2)){
+					console.log(data[i]);
+				}
 			}
 		}
-	}
-});
+	});
+}
