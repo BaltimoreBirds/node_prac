79c63549acbed10fdbd3198b9da5d4c4de9e94c4
Michael Swanson <mikeswanson12@msn.com>
2014-03-21 14:55:40 -0400

    adds official solution to lesson five files

--- learn_node5_a.js
+++ learn_node5_a.js
@@ -20,15 +20,15 @@ readAndFilter(pathBufferObject[2],pathBufferObject[3], function(err, list){
 // ------------------------------------- Official Solution---------------------------------------------
 
 
-    var filterFn = require('./solution_filter.js')
-    var dir = process.argv[2]
-    var filterStr = process.argv[3]
+    // var filterFn = require('./solution_filter.js')
+    // var dir = process.argv[2]
+    // var filterStr = process.argv[3]
 
-    filterFn(dir, filterStr, function (err, list) {
-      if (err)
-        return console.error('There was an error:', err)
+    // filterFn(dir, filterStr, function (err, list) {
+    //   if (err)
+    //     return console.error('There was an error:', err)
 
-      list.forEach(function (file) {
-        console.log(file)
-      })
-    })
+    //   list.forEach(function (file) {
+    //     console.log(file)
+    //   })
+    // })
--- learn_node5_module.js
+++ learn_node5_module.js
@@ -21,3 +21,24 @@ module.exports = function(dir, ext, callback) {
 		}
 	});
 }
+
+
+
+// ---------------------------------- Official Solution --------------------------------------------
+
+//     var fs = require('fs')
+//     var path = require('path')
+
+//     module.exports = function (dir, filterStr, callback) {
+
+//       fs.readdir(dir, function (err, list) {
+//         if (err)
+//           return callback(err)
+
+//         list = list.filter(function (file) {
+//           return path.extname(file) === '.' + filterStr
+//         })
+
+//         callback(null, list)
+//       })
+//     }
