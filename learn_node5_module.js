51f018589244fa6f1bc038759f476681054e52d4
Michael Swanson <mikeswanson12@msn.com>
2014-03-21 14:18:21 -0400

    module file function takes two arguments

--- learn_node5_module.js
+++ learn_node5_module.js
@@ -3,14 +3,14 @@ var fs = require('fs');
 
 
 
-module.exports = function(args) {
-	fs.readdir(args[2], function callback (err, data){
+module.exports = function(dir, ext) {
+	fs.readdir(dir, function callback (err, data){
 
 		if (err){
 			throw err;
 		} else{
 			for( var i = 0; i< data.length; i++ ){
-				if (args[3] == data[i].split('.').pop() && (data[i].split('.').length == 2)){
+				if (ext == data[i].split('.').pop() && (data[i].split('.').length == 2)){
 					console.log(data[i]);
 				}
 			}
