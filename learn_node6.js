d4dafa8fedca5677cf82786c2431e764b33e92e7
Michael Swanson <mikeswanson12@msn.com>
2014-03-21 18:03:39 -0400

    official solution

--- learn_node6.js
+++ learn_node6.js
@@ -19,3 +19,17 @@ http.get(pathBufferObject[2], function(response){        //response is a node ST
 		}
 	})
 });
+
+
+
+
+
+// --------------------------- Official Solution --------------------------------------
+
+//     var http = require('http')
+
+//     http.get(process.argv[2], function (response) {
+//       response.setEncoding('utf8')
+//       response.on('data', console.log)
+//       response.on('error', console.error)
+//     })
