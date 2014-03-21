0b085e2e081b960fd882fb6609ba564327398cd1
Michael Swanson <mikeswanson12@msn.com>
2014-03-21 12:44:52 -0400

    official solution documented[path module used]

--- learn_node4.js
+++ learn_node4.js
@@ -21,3 +21,14 @@ fs.readdir(pathBufferObject[2], function callback (err, data){
 });
 
 
+// Official Solution
+
+// var fs = require('fs')
+// var path = require('path')
+
+// fs.readdir(process.argv[2], function (err, list) {
+//   list.forEach(function (file) {
+//     if (path.extname(file) === '.' + process.argv[3])
+//       console.log(file)
+//   })
+// })
