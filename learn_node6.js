aca44122dbac40fe187356270f87cf4b171f59ca
Michael Swanson <mikeswanson12@msn.com>
2014-03-21 18:02:55 -0400

    my solution bitches

--- learn_node6.js
+++ learn_node6.js
@@ -1,7 +1,21 @@
 var http = require('http');
 
+var pathBufferObject = [];
+var responses = [];
+
 pathBufferObject = process.argv;
 
-http.get(pathBufferObject[2], function(response){        //response is a node STREAM object. "Stream" objects emit events. 
-
+http.get(pathBufferObject[2], function(response){        //response is a node STREAM object. "Stream" objects emit events. ≈learnyou
+	response.on("data", function(data){
+		responses.push(data.toString());
+		// console.log(data.setEncoding('utf8'));
+	})
+	response.on("error", function(data){
+		return data;
+	})
+	response.on("end", function(data){
+		for(i = 0; i<responses.length; i++){
+			console.log(responses[i]);
+		}
+	})
 });
