0c7f29aaf9a870691a9cd4bf98ff60af2235b751
Michael Swanson <mikeswanson12@msn.com>
2014-03-24 13:04:59 -0400

    adds node modules and begins lesson 8

--- learn_node7.js
+++ learn_node7.js
@@ -1,1 +1,18 @@
+var http = require('http');
 const BufferList = require('bl');
+
+var pathBufferObject = [];
+var bl = new BufferList();
+
+pathBufferObject = process.argv;
+
+http.get(pathBufferObject[2], function(response){
+	bl.pipe(response.toString());
+	console.log(bl);
+	// response.on("end", function(data){
+	// 	return bl;
+	// })
+	// bl.pipe(BufferList(function(err,data){
+	// 	data.toString();
+	// }))
+})
